1b8904d290bb130878b9f3d8bd87c2555556709a
Artyom Matveev <matveev.arty@gmail.com>
2025-08-19 16:08:43 +0700

    p1: header done, categories req added

---
+++ frontend/src/app/shared/layout/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {CategoryService} from "../../services/category.service";
+import {CategoryType} from "../../../../types/category.type";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  public categories: CategoryType[] = [];
+
+  constructor(private categoryService: CategoryService) { }
+
+  ngOnInit(): void {
+    this.categoryService.getCategories().subscribe((categories: CategoryType[]) => {
+      this.categories = categories;
+    })
+  }
+
+}
