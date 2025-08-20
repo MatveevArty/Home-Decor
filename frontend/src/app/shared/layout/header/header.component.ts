14535a7103f5103c8767c775366abd652e1db4d8
Artyom Matveev <matveev.arty@gmail.com>
2025-08-20 18:01:04 +0700

    p2: login&logout comp done, login form markup fixed

--- frontend/src/app/shared/layout/header/header.component.ts
+++ frontend/src/app/shared/layout/header/header.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {CategoryType} from "../../../../types/category.type";
+import {AuthService} from "../../../core/auth/auth.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Router} from "@angular/router";
 
 
 @Component({
@@ -9,11 +12,46 @@ import {CategoryType} from "../../../../types/category.type";
 })
 export class HeaderComponent implements OnInit {
 
+  /**
+   * Флаг факта логина пользователем
+   */
+  public isLoggedIn: boolean = false;
+
   @Input() categories: CategoryType[] = [];
 
-  constructor() { }
+  constructor(private authService: AuthService,
+              private _snackBar: MatSnackBar,
+              private router: Router,) {
+    this.isLoggedIn = this.authService.getIsLoggedIn();
+  }
 
   ngOnInit(): void {
+    this.authService.isLoggedSubject.subscribe((isLoggedIn: boolean) => {
+      this.isLoggedIn = isLoggedIn;
+    })
+  }
 
+  /**
+   * Выход из системы
+   */
+  public logout(): void {
+    this.authService.logout().subscribe({
+      next: () => {
+        this.doLogout();
+      },
+      error: () => {
+        this.doLogout();
+      }
+    });
+  }
+
+  /**
+   * Удаление токенов из localStorage и отображение попапа об успешном выходе
+   */
+  doLogout(): void {
+    this.authService.removeTokens();
+    this.authService.userId = null;
+    this._snackBar.open('Вы успешно вышли из системы');
+    this.router.navigate(['/']);
   }
 }
