3cd01df2c3ada2c820db36daabf9a4f4c3ff1f47
Artyom Matveev <matveev.arty@gmail.com>
2025-08-23 17:25:29 +0700

    p3: main-page full markup, ya-map api, main-page link in h&f

--- frontend/src/app/views/main/main.component.ts
+++ frontend/src/app/views/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ProductService} from "../../shared/services/product.service";
 import {ProductType} from "../../../types/product.type";
 import {OwlOptions} from "ngx-owl-carousel-o";
@@ -37,7 +37,63 @@ export class MainComponent implements OnInit {
     },
   }
 
-  constructor(private productService: ProductService,) { }
+  public customOptionsRev: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    margin: 26,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+    },
+  }
+
+  public reviews = [
+    {
+      name: 'Ирина',
+      image: 'review1.png',
+      text: 'В ассортименте я встретила все комнатные растения, которые меня интересовали. Цены - лучшие в городе. Доставка - очень быстрая и с заботой о растениях. ',
+    },
+    {
+      name: 'Анастасия',
+      image: 'review2.png',
+      text: 'Спасибо огромное! Цветок арека невероятно красив - просто бомба! От него все в восторге! Спасибо за сервис - все удобно сделано, доставили быстро. И милая открыточка приятным бонусом.',
+    },
+    {
+      name: 'Илья',
+      image: 'review3.png',
+      text: 'Магазин супер! Второй раз заказываю курьером, доставлено в лучшем виде. Ваш ассортимент комнатных растений впечатляет! Спасибо вам за хорошую работу!',
+    },
+    {
+      name: 'Аделина',
+      image: 'review4.jpg',
+      text: 'Хочу поблагодарить всю команду за помощь в подборе подарка для моей мамы! Все просто в восторге от мини-сада! А самое главное, что за ним удобно ухаживать, ведь в комплекте мне дали целую инструкцию.',
+    },
+    {
+      name: 'Яника',
+      image: 'review5.jpg',
+      text: 'Спасибо большое за мою обновлённую коллекцию суккулентов! Сервис просто на 5+: быстро, удобно, недорого. Что ещё нужно клиенту для счастья?',
+    },
+    {
+      name: 'Марина',
+      image: 'review6.jpg',
+      text: 'Для меня всегда важным аспектом было наличие не только физического магазина, но и онлайн-маркета, ведь не всегда есть возможность прийти на место. Ещё нигде не встречала такого огромного ассортимента!',
+    },
+  ]
+
+  constructor(private productService: ProductService,) {
+  }
 
   ngOnInit(): void {
     this.productService.getBestProducts().subscribe(
