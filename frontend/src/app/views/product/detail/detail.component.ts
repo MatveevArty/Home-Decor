4077927cb9ed946c37c7e03e8621900c9c82f58a
Artyom Matveev <matveev.arty@gmail.com>
2025-09-21 22:27:04 +0700

    p6: product page markup

--- frontend/src/app/views/product/detail/detail.component.ts
+++ frontend/src/app/views/product/detail/detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {OwlOptions} from "ngx-owl-carousel-o";
+import {ProductService} from "../../../shared/services/product.service";
+import {ProductType} from "../../../../types/product.type";
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+  /**
+   * Рекомендованные товары, получаемые с бэкенда
+   */
+  public recommendedProducts: ProductType[] = [];
+
+  /**
+   * Настройки для owl-карусели с товарами
+   */
+  public customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    margin: 24,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+  }
+
+  constructor(private productService: ProductService,) { }
 
   ngOnInit(): void {
+    this.productService.getBestProducts().subscribe(
+      (data: ProductType[]) => {
+        this.recommendedProducts = data;
+      })
   }
+
+
 
 }
