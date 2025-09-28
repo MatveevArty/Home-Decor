767fb2d29f759f5cf788fc829c4774b259d471b6
Artyom Matveev <matveev.arty@gmail.com>
2025-09-28 23:30:49 +0700

    p10: personal data autofill into order form added

--- frontend/src/app/views/order/order/order.component.ts
+++ frontend/src/app/views/order/order/order.component.ts
@@ -12,6 +12,9 @@ import {MatDialogRef} from "@angular/material/dialog/dialog-ref";
 import {OrderService} from "../../../shared/services/order.service";
 import {OrderType} from "../../../../types/order.type";
 import {HttpErrorResponse} from "@angular/common/http";
+import {UserService} from "../../../shared/services/user.service";
+import {UserInfoType} from "../../../../types/user-info.type";
+import {AuthService} from "../../../core/auth/auth.service";
 
 @Component({
   selector: 'app-order',
@@ -79,7 +82,9 @@ export class OrderComponent implements OnInit {
               private router: Router,
               private fb: FormBuilder,
               private matDialog: MatDialog,
-              private orderService: OrderService) {
+              private orderService: OrderService,
+              private userService: UserService,
+              private authService: AuthService) {
     this.updateDeliveryTypeValidation();
   }
 
@@ -100,6 +105,37 @@ export class OrderComponent implements OnInit {
 
         this.calculateTotal();
       });
+
+    if (this.authService.getIsLoggedIn()) {
+      this.userService.getUserInfo()
+        .subscribe((data: UserInfoType | DefaultResponseType) => {
+          if ((data as DefaultResponseType).error !== undefined) {
+            throw new Error((data as DefaultResponseType).message);
+          }
+
+          const userInfo = data as UserInfoType;
+
+          const paramsToUpdate = {
+            lastName: userInfo.lastName ? userInfo.lastName : '',
+            firstName: userInfo.firstName ? userInfo.firstName : '',
+            fatherName: userInfo.fatherName ? userInfo.fatherName : '',
+            phone: userInfo.phone ? userInfo.phone : '',
+            paymentType: userInfo.paymentType ? userInfo.paymentType : PaymentType.cashToCourier,
+            email: userInfo.email ? userInfo.email : '',
+            street: userInfo.street ? userInfo.street : '',
+            house: userInfo.house ? userInfo.house : '',
+            entrance: userInfo.entrance ? userInfo.entrance : '',
+            apartment: userInfo.apartment ? userInfo.apartment : '',
+            comment: '',
+          };
+
+          this.orderForm.setValue(paramsToUpdate);
+
+          if (userInfo.deliveryType) {
+            this.deliveryType = userInfo.deliveryType;
+          }
+        })
+    }
   }
 
   /**
